409799e08745a43eca0f7e31fc269a945d80753b
Michelangelo Russo <michelangelo.russo@hotmail.com>
2021-04-30 15:56:37 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/In-Progress-M-Russo/hello_go_2
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// First Go program
+	fmt.Println("Hello again, GO!!")
+}
